0188a147876a2b5b67d25b28323bd49e1d3cef68
root <root@M.localdomain>
2020-06-26 13:31:28 +0200

    part_2_ongoing

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 const express = require("express");
 const app = express();
 
+const { gettingImages } = require("./db");
+
+//-----------------------------------------------------
+//-------FILE UPLOAD BOILERPLATE-----------------------
+//-----------------------------------------------------
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, __dirname + "/uploads");
+  },
+  filename: function (req, file, callback) {
+    uidSafe(24).then(function (uid) {
+      callback(null, uid + path.extname(file.originalname));
+    });
+  },
+});
+
+const uploader = multer({
+  storage: diskStorage,
+  limits: {
+    fileSize: 2097152,
+  },
+});
+//-----------------------------------------------------
+//-------FILE UPLOAD BOILERPLATE-----------------------
+//-----------------------------------------------------
+
 app.use(express.static(__dirname + "/public"));
 
-const { gettingImages } = require("./db");
+app.post("/upload", uploader.single("file"), (req, res) => {
+  console.log("file: ", req.file); //file we just uploaded
+  console.log("input: ", req.body); //rest of the input field username, title, description
+  if (req.file) {
+    //you will do db insert here all the info
+    res.json({
+      success: true,
+    });
+  } else {
+    res.json({
+      success: false,
+    });
+  }
+});
 
 app.get("/gettingImages", (req, res) => {
   gettingImages()
     .then((results) => {
-      console.log("this is my getting results", results);
+      //console.log("this is my getting results", results);
+      //always check the result then see what you want to return from results
+      //then return it with res.json to axios to give the data back
       res.json(results.rows); //this is how you send the respond back to vue
     })
     .catch((err) => {
@@ -21,18 +66,3 @@ app.get("/gettingImages", (req, res) => {
 app.listen(8080, () => {
   console.log("my express server running!!!!");
 });
-
-/* let cities = [
-    {
-      name: "berlin",
-      country: "DE",
-    },
-    {
-      name: "mumbai",
-      country: "IND",
-    },
-    {
-      name: "london",
-      country: "UK",
-    },
-  ]; */
--- public/js/script.js
+++ public/js/script.js
@@ -5,8 +5,14 @@
       //whatever you add in data will add as property of "this".
       name: "dill",
       //values are reactive...vue automatically changes and make updates
+
+      //adding values to input field and adding it to index.html with v-mode
       seen: true,
       gettingAllInfo: [],
+      title: "",
+      description: "",
+      username: "",
+      file: null,
     },
 
     mounted: function () {
@@ -19,8 +25,34 @@
     },
 
     methods: {
-      myFunction: function () {
-        console.log("my function is running!!");
+      handleClick: function (e) {
+        e.preventDefault(); //this doesn't let the page to reload
+        console.log("my function with this:", this);
+        //we are using formdata just because we are working with the files
+
+        var formData = new FormData();
+        //appending on the data
+        formData.append("title", this.title);
+        formData.append("description", this.description);
+        formData.append("username", this.username);
+        formData.append("file", this.file);
+
+        axios
+          .post("/upload", formData)
+          .then(function (resp) {
+            console.log("resp from POST/upload: ", resp);
+          })
+          .catch(function (err) {
+            console.log("my post upload error: ", err);
+          });
+      },
+
+      handleChange: function (e) {
+        console.log("my function handlechange:");
+        console.log("file: ", e.target.files[0]);
+        //we are targeting the file and .files means the files and [0] means the selected files
+        this.file = e.target.files[0];
+        console.log("this is after adding the file: ", this);
       },
     },
   });
